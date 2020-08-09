a7eaa948a51b4f00997672995d45ae62c6f931eb
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-10 00:37:48 +0100

    starts calculating the size of the node_modules folders

--- app.js
+++ app.js
@@ -18,20 +18,50 @@ async function traverse(dirpath) {
             continue;
         } else {
             const entryPath = path.join(dirpath, entry);
-            const stat = await fs.promises.stat(entryPath);
-            if (stat.isDirectory()) {
-                if (entry === 'node_modules') {
-                    console.log(`found ${entryPath}`);
+            try {
+                const stat = await fs.promises.stat(entryPath);
+                if (stat.isDirectory()) {
+                    if (entry === 'node_modules') {
+                        console.log(`found ${entryPath}`);
+                        const size = await calculateDiskUsage(entryPath);
+                        console.log('       occupies', size);
+                    } else if (entryPath === '/Users/mac/Library') {
+                        // Ignore those files
+                    } else {
+                        await traverse(entryPath);
+                    }
+                } else if (stat.isFile()) {
+                    //console.log(`processing ${entry}`, stat);
+                    // Ignore
                 } else {
-                    await traverse(entryPath);
+                    throw new Error("this is a fucking domny shit");
                 }
-            } else if (stat.isFile()) {
-                //console.log(`processing ${entry}`, stat);
-                // Ignore
-            } else {
-                throw new Error("this is a fucking domny shit");
+            } catch {
+                // ignoring the errors to let the code runs
             }
         }
+    }
+}
+
+const calculateDiskUsage = async function (entryPath) {
+    // console.log('calculateDiskUsage ', entryPath);
+    const stat = await fs.promises.stat(entryPath);
+    if (stat.isDirectory()) {
+        // console.log(`${entryPath} is Directory`);
+        const childEntries = await fs.promises.readdir(entryPath);
+        // console.log(entryPath);
+        let total = 0;
+        for (let childEntry of childEntries) {
+            const childEntryPath = path.join(entryPath, childEntry);
+            total += await calculateDiskUsage(childEntryPath);
+        }
+        return total;
+    } else if (stat.isFile()) {
+        // console.log(`${entryPath} is a File`);
+        return stat.size;
+    } else {
+        // console.log(`neither a file or a directory`);
+        throw new Error('this is the error');
     }
 }
 
